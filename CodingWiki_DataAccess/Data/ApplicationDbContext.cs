b9f48096c99954f59eee4d843041607c309dc8d3
Zdroba Darius <dariuszdroba@gmail.com>
2023-10-18 20:05:09 +0300

    Add project files.

---
+++ CodingWiki_DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using CodingWiki_Model.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingWiki_DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-U57R7FL;Database=CodingWiki;TrustServerCertificate=True;Trusted_Connection=True;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().Property(u => u.Price).HasPrecision(10, 5);
+        }
+    }
+}
